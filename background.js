192fa3fd809145305254282f41fa9a39fb1d5386
Kochapas Santibut <kochapas@outlook.com>
2022-04-19 23:13:12 +0900

    Use storage to store auto thanks data and use popup's button to switch auto thanks between On/Off.

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
 chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ autoThanks: false });
   console.log("SB Handy Thanks installed!");
 });
--- popup.js
+++ popup.js
@@ -1,1 +1,34 @@
-console.log("Popup.js Loaded!")
+console.log("Popup.js Loaded!");
+
+const statusText = document.querySelector("#auto-on-status");
+if (statusText) {
+  chrome.storage.sync.get("autoThanks", ({ autoThanks }) => {
+    console.log("initial ==>", autoThanks);
+    if (!autoThanks) {
+      statusText.innerText = "Off";
+      statusText.style.color = "#d33";
+    } else {
+      statusText.innerText = "On";
+      statusText.style.color = "#3d3";
+    }
+  });
+}
+
+const triggerAutoBtn = document.querySelector("#trigger-auto-btn");
+if (triggerAutoBtn) {
+  console.log("switchAutoThanks ==>",);
+  triggerAutoBtn.addEventListener("click", () => {
+    chrome.storage.sync.get("autoThanks", ({ autoThanks }) => {
+      console.log("current autoThanks ==>", autoThanks);
+      if (autoThanks) {
+        statusText.innerText = "Off";
+        statusText.style.color = "#d33";
+        chrome.storage.sync.set({ autoThanks: false });
+      } else {
+        statusText.innerText = "On";
+        statusText.style.color = "#3d3";
+        chrome.storage.sync.set({ autoThanks: true });
+      }
+    });
+  });
+}
