cb97fd367ce1bd70124a1fc9094d0c70dcf6cc72
Kochapas Santibut <kochapas@outlook.com>
2022-04-19 22:25:02 +0900

    Move sayThanks tr to the top of the table to make it more 'Handy'

--- content.js
+++ content.js
@@ -1,5 +1,13 @@
 console.log("content.js Loaded!");
 
+// Move sayThanks tr to the top of the table to make it more "Handy"
+const centerTbody = document.querySelector("center tbody");
+if (centerTbody) {
+  const lastTr = centerTbody.querySelector("tr:last-child");
+  centerTbody.innerHTML = lastTr.innerHTML + centerTbody.innerHTML;
+}
+
+// Fetch the ID from URL parameter and send a "thanks" request to the server.
 const id =  new URLSearchParams(window.location.search).get("id");
 if (id) {
   const xmlHttp = new XMLHttpRequest();
