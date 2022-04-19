66856e25355314566b4d0c94745ca30a84bec6ad
Kochapas Santibut <kochapas@outlook.com>
2022-04-20 00:59:02 +0900

    Refactor codes.

--- popup.js
+++ popup.js
@@ -1,37 +1,21 @@
 console.log("Popup.js Loaded!");
 
-// const statusText = document.querySelector("#auto-on-status");
 const autoThanksCheckbox = document.getElementById("auto-thanks");
 if (autoThanksCheckbox) {
+  // Set initial checkbox value based on user's setting.
   chrome.storage.sync.get("autoThanks", ({ autoThanks }) => {
     console.log("autoThanks initial ==>", autoThanks);
-    if (!autoThanks) {
-      autoThanksCheckbox.checked = false;
-      // statusText.innerText = "Off";
-      // statusText.style.color = "#d33";
-    } else {
-      autoThanksCheckbox.checked = true;
-      // statusText.innerText = "On";
-      // statusText.style.color = "#3d3";
-    }
+    autoThanksCheckbox.checked = autoThanks ? true : false;
   });
-}
 
-if (autoThanksCheckbox) {
+  // Add event listener to autoThanksCheckbox.
   autoThanksCheckbox.addEventListener("click", () => {
     chrome.storage.sync.get("autoThanks", ({ autoThanks }) => {
       console.log("autoThanks before switch ==>", autoThanks);
-      if (autoThanks) {
-        autoThanksCheckbox.checked = false;
-        // statusText.innerText = "Off";
-        // statusText.style.color = "#d33";
-        chrome.storage.sync.set({ autoThanks: false });
-      } else {
-        autoThanksCheckbox.checked = true;
-        // statusText.innerText = "On";
-        // statusText.style.color = "#3d3";
-        chrome.storage.sync.set({ autoThanks: true });
-      }
+      const newAutoThanks = autoThanks ? false : true;
+      autoThanksCheckbox.checked = newAutoThanks;
+      chrome.storage.sync.set({ autoThanks: newAutoThanks });
+      console.log("autoThanks after switch ==>", newAutoThanks);
     });
   });
 }
